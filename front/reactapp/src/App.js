9fafe4bc34d39254b790a39b419cb114d3c30181
UZANAnthony <anthony.uzan@gmail.com>
2019-03-01 18:33:26 +0100

    front upgraded

--- front/reactapp/src/App.js
+++ front/reactapp/src/App.js
@@ -1,19 +1,67 @@
 import React, { Component } from 'react';
-
+import Gridperso from './Components/Grid';
+import Card from './Components/Card';
+import Grid from '@material-ui/core/Grid';
 import './App.css';
-import Table from './Tables';
+
+
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      child: null,
+      vide: true
+    }
+  }
+
+  myCallback = (dataFromChild) => {
+    this.setState({child: dataFromChild, vide: false})
+    console.log(dataFromChild)
+    //console.log(this.state.child)
+  }
+
   render() {
-    return (
-      <div className="App">
-        <h1>Best Hotel Selection!</h1>
-        <h2>Select a row to see details about restaurants.</h2>
-        
-        <Table />
-          
+    if(this.state.vide){
+      return(
+        <div>
+          <div className="title">
+           <h1>Choose your hotel!</h1>
+         </div>
+        <div className="container body">
+         <Grid container spacing={24}>
+          <Grid item xs={6}>
+            <Gridperso info={this.myCallback}/>  
+          </Grid>
+          <Grid item xs={6}>
+          <div class="alert alert-primary" role="alert">
+              Click on Hotel info's button!
+          </div>
+          </Grid>
+         </Grid>
+        </div>
       </div>
-    );
+      )
+    }
+    else{
+      return(
+        <div>
+          <div className="title">
+           <h1>Choose your hotel!</h1>
+         </div>
+        <div className="container body">
+         <Grid container spacing={24}>
+          <Grid item xs={6}>
+            <Gridperso info={this.myCallback}/>  
+          </Grid>
+          <Grid item xs={6}>
+          <Card item={this.state.child}/>
+          </Grid>
+         </Grid>
+        </div>
+      </div>
+      )
+    }
   }
 }
 
---
+++ front/reactapp/src/Components/Card.js
@@ -0,0 +1,134 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import classnames from 'classnames';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import red from '@material-ui/core/colors/red';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+const options = [
+  'Relais Chateau link',
+];
+
+const styles = theme => ({
+  card: {
+    maxWidth: 900,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  actions: {
+    display: 'flex',
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+});
+
+class RecipeReviewCard extends React.Component {
+  state = { expanded: false };
+
+  handleExpandClick = () => {
+    this.setState(state => ({ expanded: !state.expanded }));
+  };
+
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <Card className={classes.card}>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="Recipe" className={classes.avatar}>
+              {this.props.item.name[0]}
+            </Avatar>
+          }
+          action={
+
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
+            
+          }
+          title= {this.props.item.name}
+          subheader={this.props.item.city}
+        />
+        <CardMedia
+          className={classes.media}
+          image={this.props.item.image}
+          title="Hotel image"
+        />
+        <CardContent>
+          <Typography component="p">
+            {this.props.item.citation}
+          </Typography>
+        </CardContent>
+        <CardActions className={classes.actions} disableActionSpacing>
+          <IconButton
+            className={classnames(classes.expand, {
+              [classes.expandOpen]: this.state.expanded,
+            })}
+            onClick={this.handleExpandClick}
+            aria-expanded={this.state.expanded}
+            aria-label="Show more"
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
+        <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph>Description:</Typography>
+            <Typography paragraph>
+              {this.props.item.desc}
+            </Typography>
+            <Typography paragraph>
+              Restaurants:
+            </Typography>
+            <Typography paragraph>
+                <ul>
+                    {this.props.item.rest.map((resto) => {
+                        return (
+                            <li>
+                                {resto.name} | {resto.star}
+                            </li>
+                        )
+                    })}
+                </ul>
+            </Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
+    );
+  }
+}
+
+RecipeReviewCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(RecipeReviewCard);
